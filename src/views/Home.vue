7442e1aa40ee7071ea2b0f6b2934db45ca24528a
Martien Dermawan Tanama <martiendt@gmail.com>
2021-10-04 09:20:50 +0700

    init

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>My Lib</h1>
+    <calendar />
+  </div>
+</template>
+
+<script>
+import calendar from "vue-picker";
+export default {
+  components: {
+    calendar
+  }
+};
+</script>
+
+<style></style>
